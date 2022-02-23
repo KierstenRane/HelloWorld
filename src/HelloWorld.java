53f8ea4f661e9317e0e41ed5d3fafd096468ce47
ESPINOSAHM21 <EspinosaHM21>
2022-02-23 14:40:06 -0500

    i added content!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        String name;
+        Scanner scan = new Scanner(System.in);
+                System.out.println("what is your name user?");
+                name = scan.nextLine();
+        System.out.println("Hello " + name);
+    }
 }
